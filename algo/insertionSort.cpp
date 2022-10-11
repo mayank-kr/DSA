01fdcb2c38be81256d7943b9926749074622ca04
Kunal Khairnar <95234444+Kunal-Khairnar-05@users.noreply.github.com>
2022-10-11 22:42:37 +0530

    Add : insertionSort

---
+++ algo/insertionSort.cpp
@@ -0,0 +1,33 @@
+// Insertion Sort By Kunal Khairnar
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
